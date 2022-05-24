6c82809c27a991ff593e25d2e76650a45f4f776a
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-24 11:59:01 +0900

    実行ボタンを押した時の、空欄チェックを実装

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const App = {
       code: "",
     };
   },
+  methods: {
+    run: async function () {
+      if (this.code == "") {
+        alert("コードを入力してから押してください！");
+      } else {
+        alert("実行します！");
+      }
+    },
+  },
 };
 
 Vue.createApp(App).mount("#app");
